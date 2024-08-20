809136f0582ec2f7179d2736d245506716bf5e5f
Luigi Carannante <luigicarannante9802@libero.it>
2024-08-20 16:45:23 +0200

    add form and table

--- D2.js
+++ D2.js
@@ -3,24 +3,34 @@
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+let numb = 5;
+let numb1 = 2;
+console.log(" numb è più grande di numb1 ?", numb > numb1);
 
 /* ESERCIZIO 2
   Scrivi un algoritmo che mostri "not equal" in console se un numero intero fornito è diverso da 5.
+
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+console.log(" numb1 è diverso da 5?", numb1 != 5);
 
 /* ESERCIZIO 3
   Scrivi un algoritmo che mostri "divisibile per 5" in console se un numero fornito è perfettamente divisibile per 5 (suggerimento: usa l'operatore modulo)
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+let numb3 = 10;
+console.log(numb3 % 5);
 
 /* ESERCIZIO 4
   Scrivi un algoritmo per verificare che, dati due numeri interi, il valore di uno di essi sia 8 oppure se la loro addizione/sottrazione sia uguale a 8.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+let numb4 = 8;
+let numb5 = 16;
+console.log(numb4 === 8 || numb5 - numb4 === 8 || numb4 + numb5 === 8);
 
 /* ESERCIZIO 5
   Stai lavorando su un sito di e-commerce. Stai salvando il saldo totale del carrello dell'utente in una variabile "totalShoppingCart".
@@ -29,6 +39,12 @@
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+let totalShoppingCart = 70;
+if (totalShoppingCart > 50) {
+  console.log("spedizione gratuita per ordini superiori ai 50£ quindi il costo totale del suo ordine è:", totalShoppingCart);
+} else {
+  console.log("Il costo totale del suo ordine è:", totalShoppingCart + 10);
+}
 
 /* ESERCIZIO 6
   Stai lavorando su un sito di e-commerce. Oggi è il Black Friday e viene applicato il 20% su ogni prodotto.
@@ -36,6 +52,13 @@
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+let percTest = totalShoppingCart - (totalShoppingCart * 20) / 100;
+
+if (percTest > 50) {
+  console.log("spedizione gratuita per ordini superiori ai 50£ quindi il costo totale del suo ordine è:", percTest);
+} else {
+  console.log("Il costo totale del suo ordine è:", percTest + 10);
+}
 
 /* ESERCIZIO 7
   Crea tre variabili, e assegna un valore numerico a ciascuna di esse.
@@ -44,30 +67,44 @@
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-
+const esercizo = [11, 7, 10];
+esercizo.sort(function (a, b) {
+  return a - b;
+});
+console.log(esercizo);
 /* ESERCIZIO 8
   Crea un algoritmo per verificare che un valore fornito sia un numero oppure no (suggerimento: cerca su un motore di ricerca "typeof").
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+let name = "luigi";
+typeof "luigi";
+console.log(typeof "luigi ");
+console.log(typeof numb);
 
 /* ESERCIZIO 9
   Crea un algoritmo per controllare se un numero fornito sia pari o dispari (suggerimento: cerca l'operatore modulo su un motore di ricerca)
 */
+let num = 7;
+
+if (num % 2 === 0) {
+  console.log(num + " è un numero pari.");
+} else {
+  console.log(num + " è un numero dispari.");
+}
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
-/* ESERCIZIO 10
-  Modifica la logica del seguente algoritmo in modo che mostri in console il messaggio corretto in ogni circostanza.
-  let val = 7
-  if (val < 10) {
-      console.log("Meno di 10");
-    } else if (val < 5) {
-      console.log("Meno di 5");
-    } else {
-      console.log("Uguale a 10 o maggiore");
-    }
-*/
+/*ESERCIZIO 10
+  Modifica la logica del seguente algoritmo in modo che mostri in console il messaggio corretto in ogni circostanza.*/
+let val = 9;
+if (val < 5) {
+  console.log("Meno di 5");
+} else if (val < 10) {
+  console.log("Meno di 10");
+} else {
+  console.log("Uguale a 10 o maggiore");
+}
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
@@ -76,10 +113,13 @@
 */
 
 const me = {
-  name: 'John',
-  lastName: 'Doe',
-  skills: ['javascript', 'html', 'css'],
-}
+  name: "John",
+  lastName: "Doe",
+  skills: ["javascript", "html", "css"],
+};
+
+me.city = "toronto";
+console.log(me);
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
@@ -88,10 +128,12 @@ const me = {
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+delete me.lastName;
 
 /* ESERCIZIO 13
   Lavorando sempre sull'oggetto precedentemente fornito, scrivi del codice per rimuovere l'ultimo elemento della proprietà "skills".
 */
+me.skills.splice(2, 1);
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
@@ -100,9 +142,13 @@ const me = {
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+const numeri = [];
+numeri.push(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+console.log(numeri);
 
 /* ESERCIZIO 15
   Scrivi del codice per sostituire l'ultimo elemento dell'array, ovvero il valore 10, con il valore 100.
 */
+numeri.splice(9, 1, 100);
 
 /* SCRIVI QUI LA TUA RISPOSTA */
